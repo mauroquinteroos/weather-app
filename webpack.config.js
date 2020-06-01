8fe38e69d204320e985aa603a46b9233d0d1a87f
mauroquinteros <quinterosmauro0599@gmail.com>
2020-05-31 22:11:49 -0500

    Fixing config

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     app: path.resolve(__dirname, 'src/app/index.js')
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'js/[name].bundle.js'
   },
   module: {
